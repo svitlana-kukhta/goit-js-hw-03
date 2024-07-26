40c8d086c77995c7c7c8df18ea0e013b2175d967
Svitlana <tyschenkosvetlana@gmail.com>
2024-07-26 15:49:37 +0300

    homework-js-3

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,14 @@
 'use strict'
-function getShippingMessage(country, price, deliveryFee) {
-return `Shipping to ${country} will cost ${price + deliveryFee} credits`}
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+function makeArray(firstArray, secondArray, maxLength) {let newArray = [];
+let secondArrayLenght =  maxLength - firstArray.length;
+let secondArraySlice = secondArray.slice(0, secondArrayLenght);
+if (maxLength > 0) {newArray = firstArray.concat(secondArraySlice);
+return newArray;
+} else {return [];}}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,14 @@
 'use strict'
-function getElementWidth(content, padding, border) {
-return Number.parseInt(content) + Number.parseInt(padding) * 2 + Number.parseFloat(border) * 2;}
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+function filterArray(numbers, value) {let newArray = [];
+for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+    if(element > value) {newArray.push(element);}  
+}
+return newArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
